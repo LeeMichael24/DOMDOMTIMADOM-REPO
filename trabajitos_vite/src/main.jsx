9035ca26dbccdc9e2084df26c52bd53bb7fe3011
Mauriaguilar7 <00195721@uca.edu.sv>
2022-11-10 11:56:03 -0600

    Add: login-google

--- trabajitos_vite/src/main.jsx
+++ trabajitos_vite/src/main.jsx
@@ -8,12 +8,17 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="<560483202227-e005tg2o3les19elj9rkl728lh2d3rc4.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="560483202227-e005tg2o3les19elj9rkl728lh2d3rc4.apps.googleusercontent.com">
       <App />
-    </GoogleOAuthProvider>;
+    </GoogleOAuthProvider>
 
   </React.StrictMode>
 )
 
+//Cliente 1
 
 //560483202227-e005tg2o3les19elj9rkl728lh2d3rc4.apps.googleusercontent.com
+
+//Cliente 2
+
+//560483202227-dll86bihnpq1ra89agko3cvn9fbqihd6.apps.googleusercontent.com
