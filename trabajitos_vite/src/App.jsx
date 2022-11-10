fd0a26c68d4490d5e4c72e8c39780d994a7648bf
Mauriaguilar7 <00195721@uca.edu.sv>
2022-11-09 20:00:50 -0600

    Add: changes

--- trabajitos_vite/src/App.jsx
+++ trabajitos_vite/src/App.jsx
@@ -2,6 +2,7 @@ import classes from "./App.module.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import DOMDOMTIMADOM from "./components/Main/Main"; 
+import Feed from "./components/Feed/Feed";
 
 
 function App() {
@@ -11,7 +12,9 @@ function App() {
     {/* Header */}
     <Header />
     {/* Main */}
-
+    <main>
+      <Feed/>
+    </main>
     {/* Footer */}
 
     <Footer />
