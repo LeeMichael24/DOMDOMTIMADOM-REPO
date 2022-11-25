ff113f082d63860a470c932d2d2bec8ca0595cd3
LeeMichael24 <leemichaeln24@gmail.com>
2022-11-24 23:55:13 -0600

    Add: superficial login

--- trabajitos_vite/src/App.jsx
+++ trabajitos_vite/src/App.jsx
@@ -15,19 +15,48 @@ import FeedView from "./views/FeedView/FeedView";
 import NotFoundView from "./views/NotFoundView/NotFoundView";
 import AuthView from "./views/AuthView/AuthView";
 
+import { ValidatorRouter } from './Validator/ValidatorRouter'
+
+import { useState } from "react";
+
 
 
 function App() {
 
+  const [user, setuser] = useState(null) 
+
+  const login = () => {
+    setuser({
+      id: 1,
+      name: 'John'
+    })
+  }
+
+  const logout = () => setuser(null)
+ 
   return (
     <section className="App">
       
       <Header />
+      {
+        user ? (
+          <button onClick={logout}> Logout </button>
+        ): (
+          <button onClick={login}> Login </button>
+        )
+      }
+
       <main>
         <Routes>
           <Route path="/" element={<FeedView />}> </Route>
           <Route path="f2" element={<RecibeFormulario3 />}> </Route>
-          <Route path="fp" element={<RecibeFormularioP />}> </Route>
+          <Route path="fp" element={
+            <ValidatorRouter user={ user } redirectTo='/'>
+              <RecibeFormularioP />
+            </ValidatorRouter>
+          }/>
+
+
           <Route path="*" element={<NotFoundView />}> </Route>
           <Route path="auth/*" element={<AuthView />}> </Route>
         </Routes>
